1528c7b9efb117fa654bdf8941f3c897348f55b5
Anže Fajfar <a.fajfar95@gmail.com>
2018-11-07 00:48:13 +0100

    updating

--- Titanik-Nova/js/main.js
+++ Titanik-Nova/js/main.js
@@ -111,6 +111,20 @@ $(document).ready(function() {
         })
       });
 
+      $('.filters li').click(function(){
+        let pageLocation = window.location.pathname;
+        console.log(pageLocation);
+        if(pageLocation.includes("/menu.html")){
+            $('.filters li').removeClass('active-menu-filter');
+            $(this).addClass('active-menu-filter');
+            
+            var data = $(this).attr('data-filter');
+            $('.grid').isotope({
+            filter: data
+            });
+        }; 
+      });
+
 
 
       if(document.getElementById("food") || document.getElementById("gallery")){
@@ -118,14 +132,15 @@ $(document).ready(function() {
               itemSelector: ".all",
               percentPosition: true,
               masonry: {
-                columnWidth: ".all"
+                columnWidth: ".all",
+                gutter: 0
               }
             })
       };
 
     //------- Owl Carusel  js --------//  
 
-    $('.active-review-carusel').owlCarousel({
+    $('.-review-carusel').owlCarousel({
         items:1,
         loop:true,
         autoplay:true,
@@ -134,138 +149,33 @@ $(document).ready(function() {
         margin:30,
         dots: true
     });
+    //------- Mobile nav functionality -------//
+    let burgerBtn = document.getElementById('mobilenav-burger-btn');
+    let mobile = document.getElementsByTagName("BODY")[0];
+    let burgerBtnPressed = false;
+    burgerBtn.addEventListener('click', function() {
+        mobile.classList.toggle('navigation');
+        burgerBtnPressed = !burgerBtnPressed;
+        console.log(burgerBtnPressed);
+    }, false);
+    
 
-
-    //------- Mobile Nav  js --------//  
-
-    if ($('#nav-menu-container').length) {
-        var $mobile_nav = $('#nav-menu-container').clone().prop({
-            id: 'mobile-nav'
-        });
-        $mobile_nav.find('> ul').attr({
-            'class': '',
-            'id': ''
-        });
-        $('body .main-menu').append($mobile_nav);
-        $('body .main-menu').prepend('<span class="menu-title">Menu</span> <button type="button" id="mobile-nav-toggle"><i class="lnr lnr-menu"></i></button>');
-        $('body .main-menu').append('<div id="mobile-body-overly"></div>');
-        $('#mobile-nav').find('.menu-has-children').prepend('<i class="lnr lnr-chevron-down"></i>');
-
-        $(document).on('click', '.menu-has-children i', function(e) {
-            $(this).next().toggleClass('menu-item-active');
-            $(this).nextAll('ul').eq(0).slideToggle();
-            $(this).toggleClass("lnr-chevron-up lnr-chevron-down");
-        });
-
-        $(document).on('click', '#mobile-nav-toggle', function(e) {
-            $('body').toggleClass('mobile-nav-active');
-            $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
-            $('#mobile-body-overly').toggle();
-        });
-
-            $(document).on('click', function(e) {
-            var container = $("#mobile-nav, #mobile-nav-toggle");
-            if (!container.is(e.target) && container.has(e.target).length === 0) {
-                if ($('body').hasClass('mobile-nav-active')) {
-                    $('body').removeClass('mobile-nav-active');
-                    $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
-                    $('#mobile-body-overly').fadeOut();
-                }
-            }
-        });
-    } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
-        $("#mobile-nav, #mobile-nav-toggle").hide();
-    }
-
-    //------- Smooth Scroll  js --------//  
-
-    $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            if (target.length) {
-                var top_space = 0;
-
-                if ($('#header').length) {
-                    top_space = $('#header').outerHeight();
-
-                    if (!$('#header').hasClass('header-fixed')) {
-                        top_space = top_space;
-                    }
-                }
-
-                $('html, body').animate({
-                    scrollTop: target.offset().top - top_space
-                }, 1500, 'easeInOutExpo');
-
-                if ($(this).parents('.nav-menu').length) {
-                    $('.nav-menu .menu-active').removeClass('menu-active');
-                    $(this).closest('li').addClass('menu-active');
-                }
-
-                if ($('body').hasClass('mobile-nav-active')) {
-                    $('body').removeClass('mobile-nav-active');
-                    $('#mobile-nav-toggle i').toggleClass('lnr-times lnr-bars');
-                    $('#mobile-body-overly').fadeOut();
-                }
-                return false;
-            }
-        }
-    });
-
-    $(document).ready(function() {
-
-        $('html, body').hide();
-
-        if (window.location.hash) {
-
-            setTimeout(function() {
-
-                $('html, body').scrollTop(0).show();
-
-                $('html, body').animate({
-
-                    scrollTop: $(window.location.hash).offset().top - 108
-
-                }, 1000)
-
-            }, 0);
-
-        } else {
-
-            $('html, body').show();
-
-        }
-
-    });
-
-
-    jQuery(document).ready(function($) {
-        // Get current path and find target link
-        var path = window.location.pathname.split("/").pop();
-
-        // Account for home page with empty path
-        if (path == '') {
-            path = 'index.html';
-        }
-
-        var target = $('nav a[href="' + path + '"]');
-        // Add active class to target link
-        target.addClass('menu-active');
-    });
-
-    $(document).ready(function() {
-        if ($('.menu-has-children ul>li a').hasClass('menu-active')) {
-            $('.menu-active').closest("ul").parentsUntil("a").addClass('parent-active');
-        }
-    });
-
+    
 
 
 
     //------- Header Scroll Class  js --------//  
 
     $(window).scroll(function() {
+        console.log($(this).scrollTop());
         if($(this).scrollTop() > 100) {
+            $('#header').addClass('header-scrolled');
+            setTimeout(() => {
+                if($(this).scrollTop() > 101){
+                    $('.header-top').addClass('remove');
+                };
+            }, 250);
+        } else if ($(this).scrollTop() > 100 || burgerBtnPressed){
             $('#header').addClass('header-scrolled');
             setTimeout(() => {
                 if($(this).scrollTop() > 101){
@@ -284,8 +194,7 @@ $(document).ready(function() {
 
     window.addEventListener('scroll', () => {
         let pageLocation = window.location.pathname;
-        console.log(pageLocation);
-        if(pageLocation.includes("index.html") || pageLocation == "https://fajfara.github.io/Titanik-Nova/"){
+        if(pageLocation.includes("index.html") || pageLocation == '/'){
             let parent = document.getElementById('parallax-container');
             let children = parent.getElementsByTagName('div');
             let aboutPage = document.getElementById('about-area');
